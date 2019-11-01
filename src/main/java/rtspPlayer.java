3c030dd1e62a48b0e58936c038ad58d3a3a4a30b
Hector Trejo <h.alonsotrejo@gmail.com>
2019-11-01 16:58:10 -0600

    add control panel

--- src/main/java/rtspPlayer.java
+++ src/main/java/rtspPlayer.java
@@ -1,10 +1,12 @@
 
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.JPanel;
 
 public class rtspPlayer {
 
@@ -12,41 +14,17 @@ public class rtspPlayer {
 
         private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
         private final JFrame frame;
-        private JMenu jmenu;
-        private JMenuItem menuItem;
-        private JMenuBar menuBar;
+        private final JMenu jmenu;
+        private final JMenuItem menuItem;
+        private final JMenuBar menuBar;
+        private final JButton pauseButton;
 
 
         VideoStreaming() {
             /* Frame */
             frame = new JFrame("Hydra Media Player");
             frame.setBounds(100, 100, 600, 400);
-
-            /* Jmenu */
-            menuBar = new JMenuBar();
-            menuBar.setVisible(true);
-            jmenu = new JMenu("Archivo");
-            menuItem = new JMenuItem("Open URL");
-
-            ImageIcon img = new ImageIcon("C:\\Users\\Hector Trejo\\IdeaProjects\\rtspPlayer\\src\\main\\img\\icon2.png");
-            frame.setIconImage(img.getImage());
-
-
-
-            jmenu.add(menuItem);
-
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
-
-            menuBar.add(jmenu);
-            frame.setJMenuBar(menuBar);
-
-            menuItem.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    String streamURL = JOptionPane.showInputDialog(frame, "Ingresa la dirección URL");
-                    mediaPlayerComponent.mediaPlayer().media().play(streamURL);
-                }
-            });
 
             frame.addWindowListener(new WindowAdapter() {
                 @Override
@@ -55,18 +33,50 @@ public class rtspPlayer {
                     System.exit(0);
                 }
             });
+
+            /* Panel */
+            JPanel contentPane = new JPanel();
+            contentPane.setLayout(new BorderLayout());
+
+            mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+            contentPane.add(mediaPlayerComponent, BorderLayout.CENTER);
+
+            JPanel controlPane = new JPanel();
+            pauseButton = new JButton("Pause");
+            controlPane.add(pauseButton);
+            contentPane.add(controlPane, BorderLayout.SOUTH);
+
+            /* Jmenu */
+            menuBar = new JMenuBar();
+            menuBar.setVisible(true);
+            jmenu = new JMenu("Archivo");
+            menuItem = new JMenuItem("Open URL");
+
+            jmenu.add(menuItem);
+            menuBar.add(jmenu);
+
+            frame.setJMenuBar(menuBar);
+
+            /* App Icon */
+            ImageIcon img = new ImageIcon("C:\\Users\\Hector Trejo\\IdeaProjects\\rtspPlayer\\src\\main\\img\\icon2.png");
+            frame.setIconImage(img.getImage());
+
+            menuItem.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    String streamURL = JOptionPane.showInputDialog(frame, "Ingresa la dirección URL");
+                    mediaPlayerComponent.mediaPlayer().media().play(streamURL);
+                }
+            });
+
+            frame.setContentPane(contentPane);
+            frame.setVisible(true);
         }
 
-        void stream() {
-            frame.setContentPane(mediaPlayerComponent);
-            frame.setVisible(true);
-            //mediaPlayerComponent.mediaPlayer().media().play("rtsp://192.168.1.44:554/live/10002.sdp");
-        }
     }
 
     public static void main(String[] args) {
         VideoStreaming streaming = new VideoStreaming();
-        streaming.stream();
+        //streaming.stream();
     }
 
 
