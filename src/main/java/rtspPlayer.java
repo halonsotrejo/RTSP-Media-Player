7363659cfdc8f0c2878ad842c8774f16ced8a67b
Hector Trejo <h.alonsotrejo@gmail.com>
2020-04-03 13:43:04 -0600

    Update

--- src/main/java/rtspPlayer.java
+++ src/main/java/rtspPlayer.java
@@ -1,12 +1,18 @@
 
+
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import javax.swing.JPanel;
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class rtspPlayer {
 
@@ -17,8 +23,17 @@ public class rtspPlayer {
         private final JMenu jmenu;
         private final JMenuItem menuItem;
         private final JMenuBar menuBar;
-        private final JButton pauseButton;
+        private final JButton pauseButton,playButton,recordButton;
+        private String streamURL;
+        private boolean OnOff = false;
 
+        private final File dir = new File(System.getProperty("user.home"), "Videos");
+        //private  dir.mkdir();
+        private final DateFormat df = new SimpleDateFormat("yyyyMMdd-HHmmss");
+        private final String fileName = dir.getAbsolutePath() + "/Capture-" + df.format(new Date()) + ".mpg";
+        private final String[] options = {
+                ":sout=#transcode{vcodec=h264,vb=4096,scale=1,acodec=mpga,ab=128,channels=2,samplerate=44100}:duplicate{dst=file{dst=" + fileName + "},dst=display}"
+        };
 
         VideoStreaming() {
             /* Frame */
@@ -42,8 +57,13 @@ public class rtspPlayer {
             contentPane.add(mediaPlayerComponent, BorderLayout.CENTER);
 
             JPanel controlPane = new JPanel();
+            controlPane.setBackground(Color.BLACK);
             pauseButton = new JButton("Pause");
             controlPane.add(pauseButton);
+            playButton = new JButton("Play");
+            controlPane.add(playButton);
+            recordButton = new JButton("Grabar");
+            controlPane.add(recordButton);
             contentPane.add(controlPane, BorderLayout.SOUTH);
 
             /* Jmenu */
@@ -63,8 +83,34 @@ public class rtspPlayer {
 
             menuItem.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
-                    String streamURL = JOptionPane.showInputDialog(frame, "Ingresa la dirección URL");
+                    streamURL = JOptionPane.showInputDialog(frame, "Ingresa la dirección URL");
                     mediaPlayerComponent.mediaPlayer().media().play(streamURL);
+                }
+            });
+
+            /* Events */
+            pauseButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    mediaPlayerComponent.mediaPlayer().controls().pause();
+                }
+            });
+            playButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    mediaPlayerComponent.mediaPlayer().controls().play();
+                }
+            });
+            recordButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (OnOff != true){
+                        mediaPlayerComponent.mediaPlayer().media().start(streamURL,options);
+                        mediaPlayerComponent.setBorder(new LineBorder(Color.RED));
+                        OnOff = true;
+                    }
+                    else{
+                        mediaPlayerComponent.mediaPlayer().media().play(streamURL);
+                        mediaPlayerComponent.setBorder(new LineBorder(Color.BLACK));
+                        OnOff = false;
+                    }
                 }
             });
 
